ca2678a03be03e96cda5c0b40e65e40b2041597c
c4llv07e <kseandi@gmail.com>
2022-09-05 20:11:08 +0300

    move all code to main function
    Signed-off-by: c4llv07e <kseandi@gmail.com>

--- main.py
+++ main.py
@@ -21,49 +21,54 @@ ModeLookup = {
     "Motivity": 3
 }
 
-# Special cases
-UpdateParameter = ModuleParameter("update")
-AccelerationModeParameter = ModuleParameter("AccelerationMode")
-AccelerationMode = AccelerationModeParameter.parameterValue
-AccelerationModePlainText = "Linear"
+def main():
+    UpdateParameter = ModuleParameter("update")
+    AccelerationModeParameter = ModuleParameter("AccelerationMode")
+    AccelerationMode = AccelerationModeParameter.parameterValue
+    AccelerationModePlainText = "Linear"
+    
+    # get the acceleration mode to set the combo box
+    for mode, key in ModeLookup.items():
+        if str(AccelerationMode) == str(key):
+            AccelerationModePlainText = mode
 
-# get the acceleration mode to set the combo box
-for mode, key in ModeLookup.items():
-    if str(AccelerationMode) == str(key):
-        AccelerationModePlainText = mode
+            layout = [[sg.Text("LEETMOUSE")]]
+    layout.append([sg.Text("Mode: "),
+                   sg.Combo(["Linear", "Classic", "Motivity"],
+                            default_value=AccelerationModePlainText,
+                            enable_events=True, key="modecombo")])
 
-layout = [[sg.Text("LEETMOUSE")]]
-layout.append([sg.Text("Mode: "),
-               sg.Combo(["Linear", "Classic", "Motivity"], default_value=AccelerationModePlainText, enable_events=True, key="modecombo")])
+    for param in Parameters:
+        layout.append([sg.Text(param),
+                       sg.InputText(default_text=Parameters[param].parameterValue,
+                                    key=Parameters[param].parameterName)])
 
-for param in Parameters:
-    layout.append([sg.Text(param), sg.InputText(default_text=Parameters[param].parameterValue, key=Parameters[param].parameterName)])
+    layout.append([sg.Button("Update")])
 
-layout.append([sg.Button("Update")])
+    window = sg.Window(title="leetmouse GUI", layout=layout)
 
-window = sg.Window(title="leetmouse GUI", layout=layout)
+    while True:
+        event, values = window.read()
+        if event == "Update":
+            # Update parameters
+            for param in Parameters:
+                Parameters[param].set(window[Parameters[param].parameterName].get())
+                AccelerationModeParameter.set(str(AccelerationMode))
 
-while True:
-    event, values = window.read()
+            # Set update flag so LEETMOUSE knows to read changes
+            UpdateParameter.set("1")
 
-    if event == "Update":
-        # Update parameters
-        for param in Parameters:
-            Parameters[param].set(window[Parameters[param].parameterName].get())
-        AccelerationModeParameter.set(str(AccelerationMode))
+        if event == "modecombo":
+            # Mode was changed
+            mode = values["modecombo"]
+            # Save change but don't write it yet
+            AccelerationMode = ModeLookup[mode]
 
-        # Set update flag so LEETMOUSE knows to read changes
-        UpdateParameter.set("1")
+        if event == sg.WIN_CLOSED:
+            break
 
-    if event == "modecombo":
-        # Mode was changed
-        mode = values["modecombo"]
-        # Save change but don't write it yet
-        AccelerationMode = ModeLookup[mode]
-
-    if event == sg.WIN_CLOSED:
-        break
-
+    window.close()
 
 
-window.close()
+if __name__ == "__main__":
+    main()
